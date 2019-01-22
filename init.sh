a1a962d3afdde945f896212caa243420b5f222de
remylouisew <remylouisew@gmail.com>
2019-01-22 11:49:41 -0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y dirmngr
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+echo "deb https://dl.bintray.com/sbt/debian /" | \
+sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install -y bc scala sbt
